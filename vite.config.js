dc1e1ea9642a75084beccebbff8dc939dcfc0990
Karan Trehan <iamkarantrehan@gmail.com>
2025-06-24 01:03:33 +0530

    Ready to Deploy on Github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+
+const repoName = 'witngritlabs.github.io'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+   plugins: [react()],
+   base: '/',
 })
